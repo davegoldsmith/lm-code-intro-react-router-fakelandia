12139ff7ba476eb58a0d6b23bad02e5125883f0c
Dave Goldsmith <davegoldsmith@hotmail.com>
2023-02-24 17:11:23 +0000

    Chore: added config for test environment

---
+++ client/jest.config.ts
@@ -0,0 +1,12 @@
+import type {Config} from 'jest';
+
+const config: Config = {
+  extensionsToTreatAsEsm: ['.ts'],
+  setupFilesAfterEnv: [
+    "./src/setupTests.ts"
+  ],
+  testEnvironment: "jsdom"
+};
+
+export default config;
+
